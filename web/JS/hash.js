db1365c7d8819f7e26937d3a238f5513b9cd4c20
fenntun <fenntun@hotmail.co.uk>
2023-01-04 20:59:55 +0000

    edited to update input field in password-hasher.html

--- web/JS/hash.js
+++ web/JS/hash.js
@@ -1,8 +1,20 @@
-function sha256(string){
+async function sha256(string) {
   const encoder = new TextEncoder();
   const data = encoder.encode(string);
-  return window.crypto.subtle.digest('SHA-256',data);
+  const hashBuffer = await window.crypto.subtle.digest("SHA-256", data);
+  return hashBuffer;
 }
-function bufferToHex(buffer){
-  return Array.prototype.map.call(new Uint8Array(buffer), x => ('00' + x.toString(16)).slice(-2)).join('');
+
+async function bufferToHex(buffer) {
+  return Array.prototype.map
+    .call(new Uint8Array(buffer), (x) => ("00" + x.toString(16)).slice(-2))
+    .join("");
+}
+
+async function hashPassword() {
+  const secret_phrase = document.getElementById("secret-phrase").value;
+  const site_name = document.getElementById("site-name").value;
+  const hashBuffer = await sha256(secret_phrase + site_name);
+  const hash = await bufferToHex(hashBuffer);
+  document.getElementById("hash").value = hash;
 }
