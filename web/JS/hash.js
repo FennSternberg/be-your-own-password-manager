77c34af01b586c7a05b9300ffff0dc78251a7973
FennSternberg <94621711+FennSternberg@users.noreply.github.com>
2023-01-04 20:04:38 +0000

    Update hash.js

--- web/JS/hash.js
+++ web/JS/hash.js
@@ -1,1 +1,8 @@
-
+function sha256(string){
+  const encoder = new TextEncoder();
+  const data = encoder.encode(string);
+  return window.crypto.subtle.digest('SHA-256',data);
+}
+function buggerToHex(buffer){
+  return Array.prototype.map.call(new Uint8Array(buffer), x => ('00' + x.toString(16)).slice(-2)).join('');
+}
