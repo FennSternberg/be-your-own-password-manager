d867333b99d3f8e9b9c9f4780974a274e4788479
fenntun <fenntun@hotmail.co.uk>
2023-01-17 19:46:53 +0000

    asked chatGBT to rewrite and comment my code

--- web/JS/listeners.js
+++ web/JS/listeners.js
@@ -1,8 +1,12 @@
-window.addEventListener("load", function () {
-  clipboard = new ClipboardJS("#copy-button");
+// Listen for DOMContentLoaded event
+document.addEventListener("DOMContentLoaded", function () {
+  // Initialize clipboard
+  const clipboard = new ClipboardJS("#copy-button");
+
+  // Handle clipboard success event
   clipboard.on("success", function (e) {
     e.trigger.title = "Copied!";
-    var tooltip = new bootstrap.Tooltip(e.trigger);
+    const tooltip = new bootstrap.Tooltip(e.trigger);
     tooltip.show();
     setTimeout(() => {
       tooltip.hide();
@@ -12,34 +16,74 @@ window.addEventListener("load", function () {
     }, 300);
   });
 
+  // Listen for an input that has to be hashed
+  const hashInputs = document.querySelectorAll(".hash-pw");
+  for (let i = 0; i < hashInputs.length; i++) {
+    hashInputs[i].addEventListener("input", handleHashInput);
+  }
+
   // Listen for if sensitive information should be hidden or not
-  const secret_checkbox = document.querySelector("#secret-check");
-  secret_checkbox.addEventListener("change", (event) => {
-    const password = document.querySelector("#hash");
-    const secret_phrase = this.document.querySelector("#secret-phrase");
-    if (event.target.checked) {
-      // Hide sensitive information
-      password.classList.add("offscreen");
-      secret_phrase.type = "password";
-    } else {
-      // Show sensitive information
-      password.classList.remove("offscreen");
-      secret_phrase.classList.remove("secret");
-      secret_phrase.type = "text";
-    }
-  });
+  const secretCheckbox = document.querySelector("#secret-check");
+  secretCheckbox.addEventListener("change", handleSecretCheckbox);
 
   // Listen for if the password length should be reduced or not
-  const length_checkbox = this.document.querySelector("#length-check");
-  length_checkbox.addEventListener("change", (event) => {
-    const length = this.document.querySelector("#length-div");
+  const lengthCheckbox = document.querySelector("#length-check");
+  lengthCheckbox.addEventListener("change", handleLengthCheckbox);
+
+  // Cached DOM elements
+  const secretPhrase = document.querySelector("#secret-phrase");
+  const siteName = document.querySelector("#site-name");
+  const ending = document.querySelector("#ending");
+  const lengthCheck = document.querySelector("#length-check");
+  const passLength = document.querySelector("#length");
+  const hash = document.querySelector("#hash");
+  const lengthDiv = document.querySelector("#length-div");
+
+  /**
+   * Handles the input event for elements with the "hash-pw" class
+   */
+  async function handleHashInput() {
+    const secretPhraseValue = secretPhrase.value;
+    const siteNameValue = siteName.value.toUpperCase();
+    const hashValue = await hashPassword(secretPhraseValue, siteNameValue);
+    const endingValue = ending.value;
+    const newPassword = complyWithSite(
+      endingValue,
+      passLength,
+      lengthCheck,
+      hashValue
+    );
+    hash.innerHTML = newPassword;
+  }
+
+  /**
+   * Handles the change event for the "secret-check" checkbox
+   * @param {Event} event
+   */
+  function handleSecretCheckbox(event) {
     if (event.target.checked) {
-      // Reduce length of password
-      length.hidden = false;
+      // Hide sensitive information
+      hash.classList.add("offscreen");
+      secretPhrase.type = "password";
     } else {
-      // Length of password remains as default
-      length.hidden = true;
-      comply_with_site();
+      // Show sensitive information
+      hash.classList.remove("offscreen");
+      secretPhrase.classList.remove("secret");
+      secretPhrase.type = "text";
     }
-  });
+  }
+
+  /**
+   * Handles the change event for the "length-check" checkbox
+   * @param {Event} event
+   */
+  function handleLengthCheckbox(event) {
+    if (event.target.checked) {
+      // Show the length input
+      lengthDiv.hidden = false;
+    } else {
+      // Hide the length input
+      lengthDiv.hidden = true;
+    }
+  }
 });
